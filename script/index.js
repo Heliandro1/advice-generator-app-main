963627254206e7318e63d1d44a0cbacc904e7740
ACC <aderitocaxala10@gmail.com>
2022-12-24 18:48:56 +0100

    iniatilizing the project

---
+++ script/index.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("#btn");
+window.addEventListener("load", getAdvice);
+btn.addEventListener("click", getAdvice);
+async function getAdvice() {
+    const advice = document.querySelector("#advice");
+    try {
+        const adId = document.querySelector("#advice-id");
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const data = await res.json();
+        advice.innerText = `${data.slip.advice}`;
+        adId.innerText = `Advice #${data.slip.id}`;
+        console.log(data);
+    } catch (error) {
+        advice.innerText = "Falha na requisição"; 
+    }
+}
